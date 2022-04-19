ed6779466bb31bbdb52e6961e6986c988b5325e1
Daniel Oh <cabbachew@gmail.com>
2022-04-19 11:34:39 +0900

    Write a function that plays a single round

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function computerPlay() {
     const moves = ['Rock', 'Paper', 'Scissors'];
-    const computerSelection =  moves[Math.floor(Math.random() * moves.length)];
+    let computerSelection =  moves[Math.floor(Math.random() * moves.length)];
     return computerSelection;
 }
 
@@ -17,3 +17,49 @@ function playerPlay() {
     }   
     return playerSelection;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerPlay();
+    computerSelection = computerPlay();
+
+    let playerScore = 0;
+    let computerScore = 0;
+    if (playerSelection === computerSelection) {
+        console.log("It's a tie!");
+    } else if (playerSelection === 'Rock') {
+        switch (computerSelection) {
+            case 'Paper':
+                computerScore++;
+                console.log("You lose! Paper beats Rock.");
+                break;
+            case 'Scissors':
+                playerScore++;
+                console.log("You win! Rock beats Scissors.");
+                break;
+        }
+    } else if (playerSelection === 'Paper') {
+        switch (computerSelection) {
+            case 'Rock':
+                playerScore++;
+                console.log("You win! Paper beats Rock.");
+                break;
+            case 'Scissors':
+                computerScore++;
+                console.log("You lose! Scissors beat Paper.");
+                break;
+        }
+    } else if (playerSelection === 'Scissors') {
+        switch (computerSelection) {
+            case 'Rock':
+                computerScore++;
+                console.log("You lose! Rock beats Scissors.");
+                break;
+            case 'Paper':
+                playerScore++;
+                console.log("You win! Scissors beat Paper.");
+                break;
+        }
+    }
+    return "<Player Score :" + playerScore + "> < Computer Score: " + computerScore + ">";
+}
+
