1b9957639f6da88ba042691c3ea624d9b6e00cc3
Daniel Oh <cabbachew@gmail.com>
2022-04-19 11:06:45 +0900

    Create function to return valid player moves

--- javascript.js
+++ javascript.js
@@ -1,4 +1,19 @@
 function computerPlay() {
     const moves = ['Rock', 'Paper', 'Scissors'];
-    return moves[Math.floor(Math.random() * moves.length)];
+    const computerSelection =  moves[Math.floor(Math.random() * moves.length)];
+    return computerSelection;
+}
+
+function playerPlay() {
+    // Prompt user for move  
+    let playerInput = prompt("What's your move?");
+    let playerSelection = playerInput.toLowerCase();
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+    // Restrict user to valid moves
+    while (playerSelection !== 'Rock' && playerSelection !== 'Paper' && playerSelection !== 'Scissors') {
+        playerInput = prompt("Your move must be Rock, Paper, or Scissors");
+        playerSelection = playerInput.toLowerCase();
+        playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1);
+    }   
+    return playerSelection;
 }
